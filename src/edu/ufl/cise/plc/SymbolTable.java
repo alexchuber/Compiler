d03e6e7fcc4172923c94e30a432599bb8bd4275c
Abigailmartinez1 <73911357+Abigailmartinez1@users.noreply.github.com>
2022-03-22 16:46:19 -0400

    code from lessons... we need to edit this

--- src/edu/ufl/cise/plc/SymbolTable.java
+++ src/edu/ufl/cise/plc/SymbolTable.java
@@ -3,5 +3,22 @@ package edu.ufl.cise.plc;
 public class SymbolTable {
 
 //TODO:  Implement a symbol table class that is appropriate for this language. 
+	HashMap<String,Declaration> entries = new HashMap<>();
+	//returns true if name successfully inserted in symbol table, false if already present
+	public boolean insert(String name, Declaration declaration) 
+	{ 
+		return (entries.putIfAbsent(name,declaration) == null);
+	}
+	//returns Declaration if present, or null if name not declared.
+	public Declaration lookup(String name) 
+	{
+	     return entries.get(name);
+	} 
+	
+	SymbolTable symbolTable = new SymbolTable();
+	private void check(boolean condition, ASTNode node, String message) throws TypeCheckException 
+	{
+		if (! condition)  throw new TypeCheckException(message, node.getSourceLoc()); 
+	}
 
 }
